fd8ae7b22dc8a094e9edf3e3e29c1d58879a3384
aberash-negash <negashaberash1@gmail.com>
2023-04-08 16:25:34 -0700

    first file

---
+++ wellcome/Arthimetic_operation.java
@@ -0,0 +1,15 @@
+public class Arthimetic_operation
+{
+public static void main(String[]args)
+{
+int a,b,c;
+a=15;
+b=10;
+c=a+b;
+System.out.println("the sum is :"+c);
+c=a/b;
+System.out.println("the division is: "+c);
+c=a*b;
+System.out.println("the product is:"+c);
+}
+}
